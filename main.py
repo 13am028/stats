7c170c13756d3b304faeca4225337b7abb4205ce
13am028 <71486982+13am028@users.noreply.github.com>
2023-01-13 16:31:49 +0700

    ver2

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
+import sys
+import numpy as np
+
+
+def print_util(temp):
+    print('mean:', np.mean(temp))
+    print('std:', np.std(temp))
+    print('min:', min(temp))
+    print('max:', max(temp))
+    print('n:', len(temp))
+
 
 if __name__ == "__main__":
-    import sys
-    import numpy as np
-
     nums = []
-    with open(sys.argv[1]) as file:
-        for line in file:
-            num = line.strip()  # or some other preprocessing
-            nums.append(int(num))
+    for i in range(1, len(sys.argv)):
+        temp = []
+        path = sys.argv[i]
+        try:
+            f = open(path, 'r')
+            for line in f:
+                num = line.strip()  # or some other preprocessing
+                nums.append(int(num))
+                temp.append(int(num))
 
-    print(np.mean(nums))
-    print(np.std(nums))
-    print(min(nums))
-    print(max(nums))
+            print(path)
+            print_util(temp)
+            f.close()
+        except Exception as e:
+            print(f"Unable to open {path}: {e}", file=sys.stderr)
+
+    print('combined:')
+    print_util(nums)
